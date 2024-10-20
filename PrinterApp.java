c927c31149c6ef4f74ae0484bad4285adc865d0e
doltdev <mdashikahamed.fin@gmail.com>
2024-10-20 15:43:33 +0300

    initial commit and set up the basic

---
+++ PrinterApp.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class PrinterApp {
+    public static void main(String[] args) {
+        // Scanner to read user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Welcome message
+        System.out.println("Welcome to the Printer App!");
+    }
+}
